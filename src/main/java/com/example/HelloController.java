67572490e5a596145b5d11ef204df224189a8db5
Serguei Cambour <s.cambour@gmail.com>
2020-08-29 17:42:25 +0200

    Display app name in the HelloController
    - fix a typo in application.yml
    - add appName variable to the controller

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-    @GetMapping("hello")
+    @Value("${spring.application.name}")
+    String applicationName;
+
+    @GetMapping("/hello")
     public String sayHello(@RequestParam String greeting) {
-        return "Welcome, " + greeting + "!";
+        return applicationName + ": Welcome, " + greeting + "!";
     }
 }
