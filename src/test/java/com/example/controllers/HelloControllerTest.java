706f73dafd27c2b88d45998030b7f5cfca965b5f
Serguei Cambour <s.cambour@gmail.com>
2020-09-18 11:57:55 +0200

    Add HelloControllerTest

---
+++ src/test/java/com/example/controllers/HelloControllerTest.java
@@ -0,0 +1,29 @@
+package com.example.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class HelloControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void shouldBeAccessedWithoutToken() throws Exception {
+        this.mockMvc.perform(get("/hello").param("greeting", "John"))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.content().string(containsString("spring-demo: Welcome, John!")))
+                .andExpect(status().isOk());
+    }
+
+}
