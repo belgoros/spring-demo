74b4173ef3c6f32e5038c5874508f82e72656c0e
Serguei Cambour <s.cambour@gmail.com>
2020-09-25 17:23:37 +0200

    [WIP] Add failing test to check admion access

--- src/test/java/com/example/controllers/TestControllerTest.java
+++ src/test/java/com/example/controllers/TestControllerTest.java
@@ -1,17 +1,34 @@
 package com.example.controllers;
 
+import com.tngtech.keycloakmock.api.KeycloakMock;
+import com.tngtech.keycloakmock.api.TokenConfig;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static com.tngtech.keycloakmock.api.TokenConfig.aTokenConfig;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 class TestControllerTest {
+
+    static KeycloakMock keycloakMock = new KeycloakMock(8000, "master");
+
+    @BeforeAll
+    static void setUp() {
+        keycloakMock.start();
+    }
+
+    @AfterAll
+    static void tearDown() {
+        keycloakMock.stop();
+    }
 
     @Autowired
     private MockMvc mockMvc;
@@ -25,5 +42,18 @@ class TestControllerTest {
     @Test
     public void whenNoRolesSpecifiedThenSecuredRouteIsForbidden() throws Exception {
         mockMvc.perform(get("/test/admin")).andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void shouldRespondOKforAdminIfTokenPresent() throws Exception {
+        TokenConfig tokenConfig = aTokenConfig()
+                .withPreferredUsername("employee2")
+                .withRealmRole("ROLE_ADMIN").build();
+        String accessToken = keycloakMock.getAccessToken(tokenConfig);
+
+        mockMvc.perform(get("/test/admin")
+                .header("Authorization", "Bearer " + accessToken))
+                .andExpect(status().isOk());
+        keycloakMock.stop();
     }
 }
